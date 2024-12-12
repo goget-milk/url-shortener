29e3ed4cfbe5053c0a549180bad5c95fde6a61e1
goget-milk <mdevmann@yahoo.com>
2024-12-12 16:06:14 +0300

    initial config, logger and storage

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer 	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address 		string `yaml:"address" env-required:"true"`
+	Timeout 		time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+	
+	// check if config file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("config file is not exist: %w", configPath)
+	}
+	
+	var cfg Config
+	
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal("error reading config file: %w", err)
+	}
+	return &cfg
+}
