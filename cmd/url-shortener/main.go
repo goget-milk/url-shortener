42eda0a6de5917e616547f32e2f230756e6ebe1d
goget-milk <mdevmann@yahoo.com>
2024-12-15 02:53:36 +0300

    save and test save - ready

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
 
+	"github.com/goget-milk/url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "github.com/goget-milk/url-shortener/internal/http-server/middleware/logger"
 
 	"github.com/go-chi/chi/v5"
@@ -24,8 +26,6 @@ func main() {
 	cfg := config.MustLoad()
 	log := setupLogger(cfg.Env)
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
-	log.Debug("debug message are enabled")
-	log.Error("error message are enabled")
 
 	storage, err := sqlite.New(cfg.StoragePath)
 	if err != nil {
@@ -43,6 +43,23 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
+	router.Post("/url", save.New(log, storage))
+
+	log.Info("starting server", slog.String("address", cfg.HTTPServer.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -1,8 +1,17 @@
 package save
 
 import (
+	"errors"
 	"log/slog"
 	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+	resp "github.com/goget-milk/url-shortener/internal/lib/api/response"
+	"github.com/goget-milk/url-shortener/internal/lib/logger/sl"
+	"github.com/goget-milk/url-shortener/internal/lib/random"
+	storage "github.com/goget-milk/url-shortener/internal/stotrage"
 )
 
 type Request struct {
@@ -11,17 +20,78 @@ type Request struct {
 }
 
 type Response struct {
-	Status string `json:"status"`
-	Error  string `json:"error,omitempty"`
-	Alias  string `json:"alias,omitempty"`
+	resp.Response        // Error, OK
+	Error         string `json:"error,omitempty"`
+	Alias         string `json:"alias,omitempty"`
 }
 
 type URLSaver interface {
 	SaveURL(urlToSave string, alias string) (int64, error)
 }
 
+// TODO: maby move to  config
+const aliasLength = 6
+
 func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+		if err := render.DecodeJSON(r.Body, &req); err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+			render.JSON(w, r, resp.Error("failed to decode request body"))
+			return
+		}
+
+		log.Info("request boy decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.ValidationError(validateErrs))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, resp.Error("url already exists"))
+
+			return
+		}
+
+		if err != nil {
+			log.Error("failed to save url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to save url"))
+
+			return
+		}
+
+		log.Info("url saved", slog.Int64("id", id))
+
+		responseOK(w, r, alias)
 
 	}
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request, alias string) {
+	render.JSON(w, r, Response{
+		Response: resp.OK(),
+		Alias:    alias,
+	})
 }
